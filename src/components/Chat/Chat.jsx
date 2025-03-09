1cb587605f7c0e7ecd7a96f049ec6a6975a8ebab
Lior Lavon <liorlavon554@gmail.com>
2025-03-09 14:15:52 +0100

    1

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,13 +3,39 @@ import RoomCardSlider from "../RoomCardSlider/RoomCardSlider";
 
 import ChatMessage from "../ChatMessage/ChatMessage";
 import FilterView from "../FilterView/FilterView";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const Chat = ({ chat_flow, room_list, filterSelection, height }) => {
+  const divRef = useRef();
+
+  const randId = () => {
+    // Generate a random number between 0 and 100 (inclusive)
+    return Math.floor(Math.random() * 10000);
+  };
+
+  const scrollToBottom = () => {
+    console.log("scrollToBottom");
+
+    if (divRef.current) {
+      // divRef.current.scrollIntoView({ behavior: "smooth" });
+      setTimeout(() => {
+        divRef.current.scrollTo({
+          top: divRef.current.scrollHeight,
+          behavior: "smooth", // Smooth scroll behavior
+        });
+      }, 100);
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat_flow]);
+
   return (
     <div
       className="w-full overflow-y-auto overflow-x-hidden"
       style={{ height: `${height}px` }}
+      ref={divRef}
     >
       {chat_flow.map((chatItem) => {
         if (chatItem.type == "LOADNING") {
@@ -19,7 +45,7 @@ const Chat = ({ chat_flow, room_list, filterSelection, height }) => {
         } else if (chatItem.type == "SEARCH_RESULT") {
           return (
             <RoomCardSlider
-              key={chatItem.id}
+              key={randId()}
               room_list={room_list}
               dot_count={4}
             />
