c275fb602e88b122c21e9b1fb80a7cc5cf851ae3
Lior Lavon <liorlavon554@gmail.com>
2025-03-12 16:11:56 +0100

    add context menu

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsChat } from "react-icons/bs";
 import { RxAvatar } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
 
+const MenuItems = [
+  {
+    id: 1,
+    name: "Aaaaa",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Bbbbb",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Ccccc",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Ddddd",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Effff",
+    link: "/#",
+  },
+];
+
 const TopBar = (props) => {
   const { showAvatar, showLogin } = props;
+  const [isMenu, setIsMenu] = useState(false);
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/landing");
+  };
+
+  const handleMenu = () => {
+    setIsMenu(!isMenu);
   };
 
   const handleSignIn = () => {
@@ -16,7 +49,33 @@ const TopBar = (props) => {
 
   return (
     <div className="w-full bg-gray-100 px-2 h-12 flex flex-row items-center justify-between text-lg z-50 shadow-[0px_1px_1px_rgba(0,0,0,.2)] sans-regular">
-      <div className="text-3xl w-[20%]">{showAvatar && <RxAvatar />}</div>
+      <div className="text-3xl w-[20%]">
+        {showAvatar && (
+          <div className="group relative cursor-pointer">
+            <RxAvatar onClick={handleMenu} />
+            <div
+              className={`absolute z-[9999] ${
+                !isMenu ? "hidden" : "inline-block"
+              } w-[200px] rounded-md bg-white p-2 text-black text-base shadow-md`}
+            >
+              <ul>
+                {MenuItems.map((data) => {
+                  return (
+                    <li key={data.id} className="cursor-pointer">
+                      <a
+                        href={data.link}
+                        className="inline-block w-full rounded-md p-2"
+                      >
+                        {data.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
 
       <div className="flex items-center gap-0.5 text-[1rem]">
         <p
