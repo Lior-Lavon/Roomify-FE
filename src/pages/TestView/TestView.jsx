1d031acf8a4faaa82bc1c2b8edc94b626eddf2e7
Lior Lavon <liorlavon554@gmail.com>
2025-03-13 18:13:00 +0100

    trying to adjust screen size-4

--- src/pages/TestView/TestView.jsx
+++ src/pages/TestView/TestView.jsx
@@ -14,7 +14,7 @@ const HomeView = () => {
 
   return (
     <div
-      className={`base:hidden sm:block md:hidden w-full h-[100dvh] text-3xl sans-regular bg-white fixed  transition-all duration-250 ${
+      className={`base:hidden sm:block md:hidden w-full h-[100dvh] text-3xl sans-regular bg-white fixed ease-in-out duration-250 ${
         !isKeyboardOpen ? "top-0" : "top-[267px]"
       }`}
     >
