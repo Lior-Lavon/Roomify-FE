8d0a6569b736b518faebe110de63a85008e4d21f
Lior Lavon <liorlavon554@gmail.com>
2025-03-16 09:47:33 +0100

    add chat view

--- src/components/ChatWithOwner/ChatWithOwner.jsx
+++ src/components/ChatWithOwner/ChatWithOwner.jsx
@@ -45,16 +45,8 @@ const ChatWithOwner = ({ advertId, isVisible, closeChatWithOwner }) => {
       <TopBar showAvatar={true} showLogin={true} />
       <div className="w-full h-full flex flex-col bg-white">
         {/* TopBar */}
-        <div className="w-full h-[54px] bg-blue-300">
+        <div className="w-full h-[54px] bg-blue-300" ref={topRef}>
           <ChatWithOwnerTopBar closeChatWithOwner={closeChatWithOwner} />
-        </div>
-
-        <div className="w-full flex justify-center" ref={topRef}>
-          <RoomCardMini
-            advertInfo={advertInfo}
-            setFavorite={setFavorite}
-            shareAdvert={shareAdvert}
-          />
         </div>
 
         {/* Body (Takes up remaining space) */}
@@ -62,6 +54,14 @@ const ChatWithOwner = ({ advertId, isVisible, closeChatWithOwner }) => {
           className="flex flex-col gap-1 overflow-y-auto px-4 py-2 "
           style={{ height: `${chatHeight}px` }} // Set height dynamically
         >
+          <div className="w-full flex justify-center">
+            <RoomCardMini
+              advertInfo={advertInfo}
+              setFavorite={setFavorite}
+              shareAdvert={shareAdvert}
+            />
+          </div>
+
           {chatHistory.map((chatItem) => {
             return (
               <div
