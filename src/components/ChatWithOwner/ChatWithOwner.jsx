14698bc1cda1c2031078352279085b12c93fbc04
Lior Lavon <liorlavon554@gmail.com>
2025-03-16 16:46:14 +0100

    adding chats view

--- src/components/ChatWithOwner/ChatWithOwner.jsx
+++ src/components/ChatWithOwner/ChatWithOwner.jsx
@@ -62,7 +62,7 @@ const ChatWithOwner = ({ advertId, isVisible, closeChatWithOwner }) => {
       }`}
     >
       {/* place holder */}
-      <TopBar showAvatar={true} showLogin={true} />
+      <TopBar showAvatar={true} showLogin={false} />
       <div className="w-full h-full flex flex-col bg-white">
         {/* TopBar */}
         <div className="w-full h-[54px] bg-blue-300" ref={topRef}>
--- src/pages/Home/HomeView.jsx
+++ src/pages/Home/HomeView.jsx
@@ -5,41 +5,65 @@ import useKeyboardStatus from "../../utils/hooks/useViewportHeight";
 // fixed top-[17rem]
 const HomeView = () => {
   const { isKeyboardOpen, keyboardHeight } = useKeyboardStatus();
+  const topBarRef = useRef(null);
+  const [height, setHeight] = useState(0);
 
   // useEffect(() => {
   //   console.log("isKeyboardOpen : ", isKeyboardOpen);
   //   console.log("keyboardHeight : ", keyboardHeight);
   // }, [isKeyboardOpen, keyboardHeight]);
 
+  useEffect(() => {
+    console.log("111");
+
+    if (topBarRef.current) {
+      console.log("22");
+      const topBottom = topBarRef.current.getBoundingClientRect().bottom;
+      // const bottomTop = bottomRef.current.getBoundingClientRect().top;
+      const bottomTop = window.innerHeight;
+      setHeight(bottomTop - topBottom); // Calculate space between them
+    }
+  }, []);
+
   return (
     <div
-      className={`base:hidden sm:block md:hidden w-full h-[350px] relative text-3xl sans-regular`}
+      className={`base:hidden sm:block md:hidden w-full h-full relative text-3xl sans-regular `}
     >
-      <TopBar showAvatar={true} showLogin={true} />
-
-      <div className="mt-22 inline-block w-full text-center ">
-        <h1 className="text-transparent bg-gradient-to-r from-[#261a18] to-[#ff5733] bg-clip-text font-bold text-2lg ">
-          Welcome to Roomify
-        </h1>
-        <p className="text-[#7b7b7b] text-[15px] mt-2">
-          Find your perfect rental.
-        </p>
+      <div ref={topBarRef} className="w-full">
+        <TopBar showAvatar={true} showLogin={true} />
       </div>
 
-      <div className={`absolute  w-full mt-16`}>
-        <Prompt />
+      {/* body */}
+      <div
+        className="w-full h-full overflow-y-auto "
+        style={{ height: `${height}px` }}
+      >
+        <div className="pt-22 w-full inline-block text-center">
+          <h1 className="text-transparent bg-gradient-to-r from-[#261a18] to-[#ff5733] bg-clip-text font-bold text-2lg ">
+            Welcome to Roomify
+          </h1>
+          <p className="text-[#7b7b7b] text-[15px] mt-2">
+            Find your perfect rental.
+          </p>
+        </div>
 
-        <div className="mt-10">
-          <p className="text-center px-2 text-lg text-black">Recent listings</p>
+        <div className={`w-full mt-16`}>
+          <Prompt />
 
-          <div className="p-2 flex flex-row gap-2 overflow-auto">
-            <RoomCard />
-            <RoomCard />
-            <RoomCard />
-            <RoomCard />
-            <RoomCard />
-            <RoomCard />
-            <RoomCard />
+          <div className="mt-10">
+            <p className="text-center px-2 text-lg text-black">
+              Recent listings
+            </p>
+
+            <div className="p-2 flex flex-row gap-2 overflow-auto">
+              <RoomCard />
+              <RoomCard />
+              <RoomCard />
+              <RoomCard />
+              <RoomCard />
+              <RoomCard />
+              <RoomCard />
+            </div>
           </div>
         </div>
       </div>
