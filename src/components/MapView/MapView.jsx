532804ad87e9c520904beac88aaff90194d85931
Lior Lavon <liorlavon554@gmail.com>
2025-03-17 14:59:19 +0100

    add infoView above map

--- src/components/MapView/MapView.jsx
+++ src/components/MapView/MapView.jsx
@@ -4,11 +4,13 @@ import {
   Map,
   AdvancedMarker,
   useMap,
+  InfoWindow,
 } from "@vis.gl/react-google-maps";
+import { IoMdClose } from "react-icons/io";
 
 import OrangeMarker from "../../assets/orangeMarker.png";
 import BlackMarker from "../../assets/blackMarker.png";
-
+import Image1 from "../../assets/room1.jpeg";
 const MapView = ({ properties, visibleCardId }) => {
   return (
     <div className="w-full h-full">
@@ -24,6 +26,8 @@ export default MapView;
 const MapComponent = ({ properties, visibleCardId }) => {
   const map = useMap(); // Get the map instance
   const isUserInteracting = useRef(false); // Track manual movement
+  const [selectedMarkerId, setSelectedMarkerId] = useState(null);
+
   let defaultCenter = { lat: 52.377956, lng: 4.89707 };
 
   // Handle user moving the map manually
@@ -59,16 +63,30 @@ const MapComponent = ({ properties, visibleCardId }) => {
       // styles={mapStyle}
       onCameraChanged={handleCameraChanged} // Detect manual user movement
     >
-      <PoiMarkers properties={properties} visibleCardId={visibleCardId} />
+      <PoiMarkers
+        properties={properties}
+        visibleCardId={visibleCardId}
+        selectedMarkerId={selectedMarkerId}
+        setSelectedMarkerId={setSelectedMarkerId}
+      />
     </Map>
   );
 };
 
-const PoiMarkers = ({ properties, visibleCardId }) => {
+const PoiMarkers = ({
+  properties,
+  visibleCardId,
+  selectedMarkerId,
+  setSelectedMarkerId,
+}) => {
   return (
     <>
       {properties.map((poi) => (
-        <AdvancedMarker key={poi.Id} position={poi.Location}>
+        <AdvancedMarker
+          key={poi.Id}
+          position={poi.Location}
+          onClick={() => setSelectedMarkerId(poi.Id)}
+        >
           <div className="relative flex flex-col justify-center">
             <img
               src={poi.Id !== visibleCardId ? OrangeMarker : BlackMarker}
@@ -80,6 +98,54 @@ const PoiMarkers = ({ properties, visibleCardId }) => {
           </div>
         </AdvancedMarker>
       ))}
+
+      {/* Show InfoWindow when a marker is selected */}
+      {selectedMarkerId && (
+        <InfoWindow
+          position={
+            properties.find((poi) => poi.Id === selectedMarkerId)?.Location
+          }
+          onCloseClick={() => setSelectedMarkerId(null)} // Close when user clicks "X"
+        >
+          <div className="relative rounded-lg shadow-md ">
+            <div className="w-full flex items-center justify-end ">
+              <button
+                className="rounded-full"
+                onClick={() => setSelectedMarkerId(null)}
+              >
+                <div>
+                  <IoMdClose className="text-xl" />
+                </div>
+              </button>
+            </div>
+
+            <div className="w-full h-full">
+              <div className="flex flex-row items-center justify-center gap-2 ">
+                <img
+                  src={Image1}
+                  alt=""
+                  className="w-[5rem] h-[5rem] rounded-2xl"
+                />
+                <div className="flex flex-col">
+                  <h2 className="text-sm font-semibold">
+                    {
+                      properties.find((poi) => poi.Id === selectedMarkerId)
+                        ?.Title
+                    }
+                  </h2>
+                  <p>
+                    Price: $
+                    {
+                      properties.find((poi) => poi.Id === selectedMarkerId)
+                        ?.Price
+                    }
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </InfoWindow>
+      )}
     </>
   );
 };
