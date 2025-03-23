352544db427ad6394adb7aa7a854d34a33576926
Lior Lavon <liorlavon554@gmail.com>
2025-03-23 18:14:02 +0100

    improving profile

--- src/pages/Profile/ProfileView.jsx
+++ src/pages/Profile/ProfileView.jsx
@@ -57,154 +57,152 @@ const ProfileView = () => {
 
       {/* body */}
       <div
-        className="w-full py-2 overflow-y-auto flex flex-col gap-2 items-center"
+        className="w-full max-w-[640px] min-w-[300px] py-2 px-4 overflow-y-auto space-y-4"
         style={{ height: `${height}px` }}
       >
-        <div className="w-full max-w-[640px] min-w-[300px] px-4 mb-10 space-y-4">
-          <InputField label="Your name" value="Jacky Varsano" />
-          <InputField label="Your email" value="jackyvarsano@gmail.com" />
-          {/* <InputField label="Your password" type="password" value="*********" /> */}
-          {/* Update Password Section */}
-          <div>
-            <div
-              className="flex justify-between items-center cursor-pointer"
-              onClick={togglePasswordFields}
-            >
-              <label className="text-sm text-gray-500 block mb-2">
-                Update password
-              </label>
-              <span
-                className={`transform transition-transform duration-300 ${
-                  showPasswordFields ? "rotate-180" : ""
-                }`}
-              >
-                ▼
-              </span>
-            </div>
-            <div
-              className={`overflow-hidden transition-max-height duration-300 space-y-4 ${
-                showPasswordFields ? "max-h-50" : "max-h-0"
+        <InputField label="Your name" value="Jacky Varsano" />
+        <InputField label="Your email" value="jackyvarsano@gmail.com" />
+        {/* <InputField label="Your password" type="password" value="*********" /> */}
+        {/* Update Password Section */}
+        <div>
+          <div
+            className="flex justify-between items-center cursor-pointer"
+            onClick={togglePasswordFields}
+          >
+            <label className="text-sm text-gray-500 block mb-2">
+              Update password
+            </label>
+            <span
+              className={`transform transition-transform duration-300 ${
+                showPasswordFields ? "rotate-180" : ""
               }`}
             >
-              <InputField label="" placeholder="Password" type="password" />
-              <InputField
-                label="Retype password"
-                placeholder="Password"
-                type="password"
-              />
-            </div>
+              ▼
+            </span>
           </div>
-
-          {/* Description */}
-          <div>
-            <label className="text-sm text-gray-500 block mb-1">
-              Description
-            </label>
-            <textarea
-              className="w-full rounded-2xl border border-gray-300 p-3 text-sm"
-              rows={3}
-              defaultValue="About my self About my self About my self About my self"
+          <div
+            className={`overflow-hidden transition-max-height duration-300 space-y-4 ${
+              showPasswordFields ? "max-h-50" : "max-h-0"
+            }`}
+          >
+            <InputField label="" placeholder="Password" type="password" />
+            <InputField
+              label="Retype password"
+              placeholder="Password"
+              type="password"
             />
           </div>
-
-          {/* <InputField label="Phone number" vsalue="0625484441" /> */}
-          {/* <InputField label="Your address" value="Boogschutter 12" /> */}
-          <InputField label="City" value="Amstelveen" />
-
-          {/* Profile Type Toggle */}
-          <div>
-            <label className="text-sm text-gray-500 block mb-2">
-              Profile type
-            </label>
-            <div className="flex gap-2">
-              <button
-                className={`flex-1 py-2 rounded-full text-sm ${
-                  profileType === "Tenant"
-                    ? "bg-orange-600 text-white font-medium"
-                    : "bg-gray-100 text-gray-400"
-                }`}
-                onClick={() => handleProfileTypeChange("Tenant")}
-              >
-                Tenant
-              </button>
-              <button
-                className={`flex-1 py-2 rounded-full text-sm ${
-                  profileType === "Landlord"
-                    ? "bg-orange-600 text-white font-medium"
-                    : "bg-gray-100 text-gray-400"
-                }`}
-                onClick={() => handleProfileTypeChange("Landlord")}
-              >
-                Landlord
-              </button>
-            </div>
-            {profileType == "Tenant" && (
-              <div className="w-full space-y-4 mt-4">
-                {/* Date of Birth */}
-                <InputField label="Date of birth" type="date" />
-                {/* Nationality */}
-                <SelectField
-                  label="Nationality"
-                  options={["Arab", "Dutch", "French", "Other"]}
-                />
-                {/* Owner Type */}
-                <SelectField
-                  label="Owner type"
-                  options={[
-                    "Roommate",
-                    "Private Owner",
-                    "Real Estate Agent",
-                    "N/A",
-                  ]}
-                />
-
-                {/* Speaks Languages (Searchable Dropdown with Tags) */}
-                <SearchableMultiSelectField
-                  label="Speaks languages"
-                  options={[
-                    "English",
-                    "Dutch",
-                    "Arabic",
-                    "Spanish",
-                    "French",
-                    "German",
-                    "Italian",
-                  ]}
-                />
-                {/* Living in Country */}
-                <SelectField
-                  label="Living in country"
-                  options={["Netherlands", "Germany", "France", "Spain"]}
-                />
-                {/* Status */}
-                <SelectField
-                  label="Status"
-                  options={[
-                    "Student",
-                    "Working Student",
-                    "Working",
-                    "Looking for a job",
-                  ]}
-                />
-                {/* Has a Pet */}
-                <YesNoToggle label="Has a pet" />
-                {/* Smoking Inside */}
-                <YesNoToggle label="Smoking inside" />
-                {/* Member of Student Association */}
-                <YesNoToggle label="Member of student association" />
-              </div>
-            )}
-          </div>
-
-          <div className="flex gap-2">
-            <button
-              className={`flex-1 py-2 rounded-full text-sm bg-red-50 text-red-500 font-medium`}
-              onClick={() => {}}
-            >
-              Update
-            </button>
-          </div>
         </div>
+
+        {/* Description */}
+        <div>
+          <label className="text-sm text-gray-500 block mb-1">
+            Description
+          </label>
+          <textarea
+            className="w-full rounded-2xl border border-gray-300 p-3 text-sm"
+            rows={3}
+            defaultValue="About my self About my self About my self About my self"
+          />
+        </div>
+
+        {/* <InputField label="Phone number" vsalue="0625484441" /> */}
+        {/* <InputField label="Your address" value="Boogschutter 12" /> */}
+        <InputField label="City" value="Amstelveen" />
+
+        {/* Profile Type Toggle */}
+        <label className="text-sm text-gray-500 block mb-2">Profile type</label>
+
+        <div className="flex gap-2">
+          <button
+            className={`flex-1 py-2 rounded-full text-sm ${
+              profileType === "Tenant"
+                ? "bg-orange-600 text-white font-medium"
+                : "bg-gray-100 text-gray-400"
+            }`}
+            onClick={() => handleProfileTypeChange("Tenant")}
+          >
+            Tenant
+          </button>
+          <button
+            className={`flex-1 py-2 rounded-full text-sm ${
+              profileType === "Landlord"
+                ? "bg-orange-600 text-white font-medium"
+                : "bg-gray-100 text-gray-400"
+            }`}
+            onClick={() => handleProfileTypeChange("Landlord")}
+          >
+            Landlord
+          </button>
+        </div>
+
+        {/* Tenant properties */}
+        {profileType == "Tenant" && (
+          <div className="w-full space-y-4 mt-4">
+            {/* Date of Birth */}
+            <InputField label="Date of birth" type="date" />
+            {/* Nationality */}
+            <SelectField
+              label="Nationality"
+              options={["Arab", "Dutch", "French", "Other"]}
+            />
+            {/* Owner Type */}
+            <SelectField
+              label="Owner type"
+              options={[
+                "Roommate",
+                "Private Owner",
+                "Real Estate Agent",
+                "N/A",
+              ]}
+            />
+
+            {/* Speaks Languages (Searchable Dropdown with Tags) */}
+            <SearchableMultiSelectField
+              label="Speaks languages"
+              options={[
+                "English",
+                "Dutch",
+                "Arabic",
+                "Spanish",
+                "French",
+                "German",
+                "Italian",
+              ]}
+            />
+            {/* Living in Country */}
+            <SelectField
+              label="Living in country"
+              options={["Netherlands", "Germany", "France", "Spain"]}
+            />
+            {/* Status */}
+            <SelectField
+              label="Status"
+              options={[
+                "Student",
+                "Working Student",
+                "Working",
+                "Looking for a job",
+              ]}
+            />
+            {/* Has a Pet */}
+            <YesNoToggle label="Has a pet" />
+            {/* Smoking Inside */}
+            <YesNoToggle label="Smoking inside" />
+            {/* Member of Student Association */}
+            <YesNoToggle label="Member of student association" />
+          </div>
+        )}
+
+        <div className="flex gap-2">
+          <button
+            className={`flex-1 py-2 rounded-full text-sm bg-red-50 text-red-500 font-medium`}
+            onClick={() => {}}
+          >
+            Update
+          </button>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
