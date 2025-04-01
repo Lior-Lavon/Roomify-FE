d2d620cb83c24dcc9116ee395192c24f390eed2a
Lior Lavon <liorlavon554@gmail.com>
2025-04-01 16:43:59 +0200

    1

--- src/components/RoomCard/RoomCardMini.jsx
+++ src/components/RoomCard/RoomCardMini.jsx
@@ -43,19 +43,19 @@ const RoomCardMini = memo(
     return (
       <div
         ref={cardRef}
-        className={`h-[6rem] border border-orange-200 rounded-xl flex flex-row gap-2 items-center justify-between shadow-[10px_2px_12px_rgba(0,0,0,.1)] `}
+        className={`h-[100px] border border-orange-200 rounded-xl flex flex-row gap-2 items-center justify-between shadow-[10px_2px_12px_rgba(0,0,0,.1)] `}
         style={{
           width: screenWidth,
         }}
         onClick={onAdvertClick}
       >
         <div className="w-full h-full flex flex-col justify-between gap-1 pl-2">
-          <div className="w-full max-w-[350px] whitespace-normal break-words flex flex-col gap-1">
+          <div className="w-full max-w-[350px] whitespace-normal break-words flex flex-col  pt-[2px]">
             <p className="text-[13px] font-bold">{advertInfo?.Title}</p>
             <p className="text-[11px]">{advertInfo?.Address}</p>
           </div>
 
-          <div className="w-full flex items-center justify-between">
+          <div className="w-full flex items-center justify-between pb-1">
             <p className="text-[10px]">
               <span className="text-orange-600 text-sm sans-bold">
                 ${advertInfo?.Price + " "}
@@ -82,7 +82,7 @@ const RoomCardMini = memo(
         {advertInfo?.Images.length >= 1 && (
           <img
             src={advertInfo?.Images[0]}
-            className="w-[7.2rem] h-[6rem] object-cover rounded-tr-lg rounded-br-lg"
+            className="w-[7.2rem] h-[100px] object-cover rounded-tr-lg rounded-br-lg"
           />
         )}
       </div>
